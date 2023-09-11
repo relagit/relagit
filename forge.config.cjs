f78a5811b55fab75071b029406abf22922565834
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-09-11 17:20:29 +1000

    (forge) chore: Move forge config

---
+++ forge.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+	packagerConfig: {
+		name: 'RelaGit',
+		appBundleId: 'com.rela.git',
+		icon: './icons/dark'
+	},
+	makers: [
+		{
+			name: '@electron-forge/maker-squirrel',
+			config: {
+				name: 'electron_quick_start'
+			}
+		},
+		{
+			name: '@electron-forge/maker-zip',
+			platforms: ['darwin']
+		},
+		{
+			name: '@electron-forge/maker-deb',
+			config: {}
+		},
+		{
+			name: '@electron-forge/maker-rpm',
+			config: {}
+		}
+	]
+};
