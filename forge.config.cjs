d1b161c55766e05713e1371b8836904e54a27730
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-09-26 14:10:59 +1000

    (forge) add dmg for macos and update makers

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,6 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const build = require('./dist/build_info.json');
+const path = require('path');
+const fs = require('fs');
+
+const out = {
+	macos: fs.existsSync(path.join(__dirname, 'out', 'RelaGit-darwin-x64', 'RelaGit.app'))
+		? path.join(__dirname, 'out', 'RelaGit-darwin-x64', 'RelaGit.app')
+		: path.join(__dirname, 'out', 'RelaGit-darwin-arm64', 'RelaGit.app')
+};
 
 module.exports = {
 	packagerConfig: {
@@ -10,22 +18,46 @@ module.exports = {
 	},
 	makers: [
 		{
-			name: '@electron-forge/maker-squirrel',
+			name: '@electron-forge/maker-dmg',
+			platforms: ['darwin'],
 			config: {
-				name: 'electron_quick_start'
+				background: './icons/dmg-background.png',
+				format: 'ULFO',
+				name: 'RelaGit',
+				overwrite: true,
+				icon: './icons/light.icns',
+				iconSize: 72,
+				additionalDMGOptions: {
+					'background-color': '#000'
+				},
+				contents: [
+					{ x: 462, y: 160, type: 'link', path: '/Applications' },
+					{
+						x: 155,
+						y: 160,
+						type: 'file',
+						path: out.macos
+					}
+				]
 			}
 		},
 		{
-			name: '@electron-forge/maker-zip',
-			platforms: ['darwin']
+			name: '@electron-forge/maker-squirrel',
+			config: {
+				name: 'RelaGit'
+			}
 		},
 		{
 			name: '@electron-forge/maker-deb',
-			config: {}
+			config: {
+				name: 'relagit'
+			}
 		},
 		{
 			name: '@electron-forge/maker-rpm',
-			config: {}
+			config: {
+				name: 'relagit'
+			}
 		}
 	]
 };
