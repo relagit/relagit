3e415e2245102043dbb74384849aeed2ae9b87b4
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-09-26 21:04:49 +1000

    (ui/Sidebar/Header) add context menu

--- packages/app/src/ui/Sidebar/Header/index.tsx
+++ packages/app/src/ui/Sidebar/Header/index.tsx
@@ -1,10 +1,12 @@
 import { createSignal, Show } from 'solid-js';
 
+import { showItemInFolder } from '@app/modules/shell';
 import { createStoreListener } from '@stores/index';
 import RespositoryStore from '@stores/repository';
 import LocationStore from '@stores/location';
 
 import Icon from '@ui/Common/Icon';
+import Menu from '@app/ui/Menu';
 import Drawer from './Drawer';
 
 import './index.scss';
@@ -23,37 +25,60 @@ export default () => {
 	return (
 		<>
 			<Drawer open={[open, setOpen]} />
-			<div
-				aria-role="button"
-				aria-label="Open Repository Drawer"
-				tabIndex={0}
-				class="sidebar__header"
-				onClick={() => setOpen(!open())}
-				onKeyDown={(e) => {
-					if (e.key === 'Enter') {
-						setOpen(!open());
+			<Menu
+				items={[
+					{
+						type: 'item',
+						label: `View in ${
+							window.Native.platform === 'darwin' ? 'Finder' : 'Explorer'
+						}`,
+						onClick: () => {
+							showItemInFolder(selected().path);
+						},
+						disabled: !selected()
+					},
+					{
+						label: 'Open in Code',
+						disabled: !selected(),
+						type: 'item'
 					}
-				}}
+				]}
 			>
-				<div class="sidebar__header__info">
-					<div class="sidebar__header__repository">
-						{selected()?.name || 'No Repository Selected'}
-					</div>
-					<div class="sidebar__header__details">
-						<Show when={selected()} fallback={<span>Choose one to get started.</span>}>
-							<span class="sidebar__header__details__branch">
-								{selected()?.branch || 'No Branch'}
-							</span>
-						</Show>
-					</div>
-				</div>
 				<div
-					class="sidebar__header__chevron"
-					style={{ transform: `rotate(${open() ? 90 : -90}deg)` }}
+					aria-role="button"
+					aria-label="Open Repository Drawer"
+					tabIndex={0}
+					class="sidebar__header"
+					onClick={() => setOpen(!open())}
+					onKeyDown={(e) => {
+						if (e.key === 'Enter') {
+							setOpen(!open());
+						}
+					}}
 				>
-					<Icon name="chevron-down" />
+					<div class="sidebar__header__info">
+						<div class="sidebar__header__repository">
+							{selected()?.name || 'No Repository Selected'}
+						</div>
+						<div class="sidebar__header__details">
+							<Show
+								when={selected()}
+								fallback={<span>Choose one to get started.</span>}
+							>
+								<span class="sidebar__header__details__branch">
+									{selected()?.branch || 'No Branch'}
+								</span>
+							</Show>
+						</div>
+					</div>
+					<div
+						class="sidebar__header__chevron"
+						style={{ transform: `rotate(${open() ? 90 : -90}deg)` }}
+					>
+						<Icon name="chevron-down" />
+					</div>
 				</div>
-			</div>
+			</Menu>
 		</>
 	);
 };
