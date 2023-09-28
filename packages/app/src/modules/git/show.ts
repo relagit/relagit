65b378f1c7ca300a9dc67e0aee16eb3bfb8f13b6
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-09-28 14:50:00 +1000

    (modules/git/show) fix diff splitting

--- packages/app/src/modules/git/show.ts
+++ packages/app/src/modules/git/show.ts
@@ -29,7 +29,7 @@ export const Show = async (repository: string, hash: string): Promise<IPastCommi
 	const res = await Git({
 		directory: repository,
 		command: 'show',
-		args: [hash, '--pretty=format:']
+		args: [hash, '"--pretty=format:"']
 	});
 
 	const commit: IPastCommit = {
@@ -37,20 +37,31 @@ export const Show = async (repository: string, hash: string): Promise<IPastCommi
 		files: []
 	};
 
-	const files = res.split('diff --git ');
+	const files = res.split(/^diff --git/gm); // adding the "^" should ensure we are never splitting in the middle of a file
 
 	for (let i = 0; i < files.length; i++) {
 		const file = files[i];
 
 		if (file.length === 0) continue;
 
-		const [filename, ...diff] = file.split('\n');
+		const [name, ...diff] = file.split('\n');
+
+		const _diff = parse('diff --git ' + name + '\n' + diff.join('\n') + '');
 
 		commit.files.push({
-			filename: path.basename(filename.replace('a/', '').split(' b/').pop()),
-			path: path.dirname(filename.replace('a/', '').split(' b/').pop()),
-			diff: parse('diff --git ' + filename + '\n' + diff.join('\n')),
-			status: 'unknown'
+			filename: path.basename(name.replace('a/', '').split(' b/').pop()),
+			path: path.dirname(name.replace('a/', '').split(' b/').pop()),
+			diff: _diff,
+			status:
+				_diff.files[0]?.type === 'ChangedFile'
+					? 'modified'
+					: _diff.files[0]?.type === 'AddedFile'
+					? 'added'
+					: _diff.files[0]?.type === 'DeletedFile'
+					? 'deleted'
+					: _diff.files[0]?.type === 'RenamedFile'
+					? 'renamed'
+					: 'unknown'
 		});
 	}
 
