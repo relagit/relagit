d153348fa36c039b25c7f052a99fc881476565ad
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-09-28 11:07:51 +1000

    (ipc, ui/Modal) fix reload button

--- packages/common/ipc.ts
+++ packages/common/ipc.ts
@@ -3,4 +3,5 @@ export const OPEN_SWITCHER = 'window:open-switcher';
 export const OPEN_SIDEBAR = 'window:open-sidebar';
 export const LOAD_WORKFLOW = 'window:load-workflow';
 export const OPEN_SETTINGS = 'window:open-settings';
+export const RELOAD_CLIENT = 'window:reload-client';
 export const FOCUS = 'window:focus';
