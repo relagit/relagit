cc7fee21aca8f7db106fd70c3ead3b4d963bbc82
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-11-01 15:15:16 +1100

    (modules/focus) improve/fix focus trap

--- packages/app/src/modules/focus.ts
+++ packages/app/src/modules/focus.ts
@@ -1,73 +1,24 @@
 import { createEffect, Accessor } from 'solid-js';
-
-const tabbableSelectors = [
-	'a[href]',
-	'area[href]',
-	'input',
-	'select',
-	'textarea',
-	'button',
-	'iframe',
-	'object',
-	'embed',
-	'*[tabindex]',
-	'*[contenteditable]'
-].join(', ');
-
-const clean = (elements: NodeListOf<HTMLElement>) => {
-	return Array.from(elements).filter(
-		(el) =>
-			!!(el.offsetWidth || el.offsetHeight || el.getClientRects().length) && !el['disabled']
-	);
-};
+import * as ft from 'focus-trap';
 
 export const createFocusTrap = (active: Accessor<boolean>, el: Accessor<HTMLElement | null>) => {
-	let focusableElements: HTMLElement[] = [];
-
-	let nextFocusableElement: HTMLElement | null = null;
-	let previousFocusableElement: HTMLElement | null = null;
+	let trap: ft.FocusTrap | null = null;
 
 	createEffect(() => {
+		trap?.deactivate();
+
 		if (!active()) return;
 
 		const node = el();
 
 		if (!node) return;
 
-		focusableElements = Array.from(clean(node.querySelectorAll(tabbableSelectors)));
+		trap = ft.createFocusTrap(node, {
+			clickOutsideDeactivates: false,
+			escapeDeactivates: false,
+			returnFocusOnDeactivate: false
+		});
 
-		nextFocusableElement = focusableElements[0];
-		previousFocusableElement = focusableElements[focusableElements.length - 1];
-
-		const handleKeyDown = (e: KeyboardEvent) => {
-			if (e.key === 'Tab') {
-				e.preventDefault();
-
-				focusableElements = Array.from(clean(node.querySelectorAll(tabbableSelectors)));
-
-				if (focusableElements.indexOf(nextFocusableElement) === -1) {
-					nextFocusableElement = focusableElements[0];
-					previousFocusableElement = focusableElements[focusableElements.length - 1];
-				}
-
-				if (focusableElements.length === 0) return;
-
-				if (e.shiftKey) {
-					previousFocusableElement?.focus();
-
-					nextFocusableElement = previousFocusableElement;
-					previousFocusableElement =
-						focusableElements[focusableElements.indexOf(previousFocusableElement) - 1];
-				} else {
-					nextFocusableElement?.focus();
-
-					previousFocusableElement = nextFocusableElement;
-					nextFocusableElement =
-						focusableElements[focusableElements.indexOf(nextFocusableElement) + 1];
-				}
-			}
-		};
-
-		node.addEventListener('keydown', handleKeyDown);
+		trap.activate();
 	});
 };
