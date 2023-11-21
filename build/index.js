2f8444067a7337b8f11e38d4fa2f8963e8b69394
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-11-21 22:08:31 +1100

    (workflows) custom build script

---
+++ build/index.js
@@ -0,0 +1,47 @@
+import child_process from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const GITHUB_TOKEN = process.argv[2].replace('--token=', '');
+
+const platform = () => {
+	switch (process.platform) {
+		case 'darwin':
+			return 'mac';
+		case 'win32':
+			return 'win';
+		case 'linux':
+			return 'linux';
+		default:
+			throw new Error('Unknown platform');
+	}
+};
+
+const run = () => {
+	console.log('checking if node_modules exists');
+	if (!fs.existsSync(path.resolve(__dirname, '../node_modules'))) {
+		console.log("it doesn't! installing now...");
+		child_process.execSync('pnpm i --frozen-lockfile', {
+			cwd: path.resolve(__dirname, '..')
+		});
+	} else {
+		console.log('it does!');
+	}
+
+	console.log('building...');
+	child_process.execSync('pnpm build', {
+		cwd: path.resolve(__dirname, '..')
+	});
+
+	console.log('making...');
+	child_process.execSync(`pnpm make:${platform()} --publish always`, {
+		cwd: path.resolve(__dirname, '..'),
+		env: {
+			GH_TOKEN: GITHUB_TOKEN
+		}
+	});
+};
+
+run();
