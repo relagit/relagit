8dcbd99fe461aa897e0b451a0d504622694e7cc6
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-11-27 14:11:49 +1100

    (ui/Common/Popout) add popout component

---
+++ packages/app/src/ui/Common/Popout/index.tsx
@@ -0,0 +1,128 @@
+import { JSX, Setter, Show, createSignal, onCleanup, onMount } from 'solid-js';
+import { Portal } from 'solid-js/web';
+import { Transition } from 'solid-transition-group';
+
+import './index.scss';
+
+export interface IPopout {
+	children: (p: {
+		show: (e: MouseEvent | KeyboardEvent) => void;
+		toggle: (e: MouseEvent | KeyboardEvent) => void;
+		hide: () => void;
+		ref: Setter<HTMLElement>;
+	}) => JSX.Element | JSX.Element[];
+	body: (p: {
+		show: (e: MouseEvent | KeyboardEvent) => void;
+		toggle: (e: MouseEvent | KeyboardEvent) => void;
+		hide: () => void;
+	}) => JSX.Element | JSX.Element[];
+	position?: 'top' | 'bottom' | 'auto';
+}
+
+export default (props: IPopout) => {
+	const [open, setOpen] = createSignal(false);
+	const [x, setX] = createSignal(0);
+	const [y, setY] = createSignal(0);
+
+	const [wrapper, setWrapper] = createSignal<HTMLElement>();
+	const [popout, setPopout] = createSignal<HTMLElement>();
+
+	const listener = () => {
+		const rect = wrapper()?.getBoundingClientRect();
+
+		setX(rect.left + rect.width / 2);
+		setY(rect.top + rect.height / 2);
+	};
+
+	onMount(() => {
+		window.addEventListener('resize', listener);
+	});
+
+	onCleanup(() => {
+		window.removeEventListener('resize', listener);
+	});
+
+	const hide = () => {
+		setOpen(false);
+	};
+
+	const show = (e: MouseEvent | KeyboardEvent) => {
+		e.stopPropagation();
+
+		console.log(wrapper(), popout());
+
+		setOpen(true);
+
+		const rect = wrapper()?.getBoundingClientRect();
+
+		setX(rect.left + rect.width / 2);
+		setY(rect.top + rect.height / 2);
+	};
+
+	const toggle = (e: MouseEvent | KeyboardEvent) => {
+		e.stopPropagation();
+
+		if (open()) {
+			hide();
+		} else {
+			show(e);
+		}
+	};
+
+	return (
+		<>
+			<props.children
+				hide={hide}
+				toggle={toggle}
+				show={show}
+				ref={setWrapper}
+			></props.children>
+			<Portal mount={document.getElementById('app-container')}>
+				<Transition
+					onEnter={(el, done) => {
+						const a = el.animate(
+							{
+								opacity: [0, 1],
+								translate: ['0 4px', '0 0px'],
+								scale: [0.95, 1]
+							},
+							{
+								duration: 200,
+								easing: 'ease-out'
+							}
+						);
+
+						a.finished.then(done);
+					}}
+					onExit={(el, done) => {
+						const a = el.animate(
+							{
+								opacity: [1, 0],
+								translate: ['0 0px', '0 4px'],
+								scale: [1, 0.95]
+							},
+							{
+								duration: 200,
+								easing: 'ease-out'
+							}
+						);
+
+						a.finished.then(done);
+					}}
+				>
+					<Show when={open()}>
+						<div
+							class={`popout ${props.position ? props.position : 'top'}`}
+							ref={setPopout}
+							style={`--x: ${x()}px; --y: ${y()}px; --w-h: ${wrapper()
+								?.offsetHeight}px; --w-w: ${wrapper()?.offsetWidth}px;`}
+						>
+							<props.body hide={hide} toggle={toggle} show={show} />
+							<div class="popout__arrow"></div>
+						</div>
+					</Show>
+				</Transition>
+			</Portal>
+		</>
+	);
+};
