67219d67a34b4c2684e462de94be0b296787d2d5
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-12-21 14:56:07 +0000

    (native) fix usage of updateEnvironmentForProcess

--- packages/native/src/index.ts
+++ packages/native/src/index.ts
@@ -8,7 +8,7 @@ import pkj from '../../../package.json' assert { type: 'json' };
 import initIPC, { dispatch } from './modules/ipc';
 import { log } from './modules/logger';
 import { backgroundFromTheme, getSettings, setSettings } from './modules/settings';
-import { shellNeedsPatching, updateEnvironmentForProcess } from './modules/shell';
+import { updateEnvironmentForProcess } from './modules/shell';
 
 app.setAboutPanelOptions({
 	applicationName: 'RelaGit',
@@ -17,10 +17,6 @@ app.setAboutPanelOptions({
 	copyright: 'Copyright © 2023 TheCommieAxolotl & RelaGit contributors',
 	website: 'https://rela.dev'
 });
-
-if (shellNeedsPatching(process)) {
-	updateEnvironmentForProcess();
-}
 
 const constructWindow = async () => {
 	let settings: Map<string, unknown>;
@@ -113,10 +109,6 @@ const constructWindow = async () => {
 		settings.set('window.height', win.getSize()[1]);
 
 		setSettings(settings);
-	});
-
-	win.once('ready-to-show', () => {
-		win.show();
 	});
 
 	win.on('focus', () => {
@@ -285,6 +277,7 @@ app.once('ready', async () => {
 	const window = await constructWindow();
 
 	initIPC(window);
+	if (process.platform === 'darwin') updateEnvironmentForProcess();
 
 	app.on('activate', async () => {
 		if (BrowserWindow.getAllWindows().length === 0) {
--- packages/native/src/modules/shell.ts
+++ packages/native/src/modules/shell.ts
@@ -3,7 +3,6 @@
  * https://github.com/desktop/desktop/blob/development/app/src/lib/shell.ts
  */
 import child_process, { ExecFileOptions } from 'child_process';
-import os from 'os';
 import { promisify } from 'util';
 
 import { error } from './logger';
@@ -11,43 +10,6 @@ import { error } from './logger';
 const execFile = promisify(child_process.execFile);
 
 const ExcludedEnvironmentVars = new Set(['LOCAL_GIT_DIRECTORY']);
-
-const getSystemVersionSafe = () => {
-	if (process.platform === 'darwin') {
-		// getSystemVersion only exists when running under Electron, and not when
-		// running unit tests which frequently end up calling this. There are no
-		// other known reasons why getSystemVersion() would return anything other
-		// than a string
-		return 'getSystemVersion' in process ? process.getSystemVersion() : undefined;
-	} else {
-		return os.release();
-	}
-};
-
-const isMacOSCatalinaOrEarlier = () => {
-	const version = getSystemVersionSafe();
-
-	if (!version) {
-		return false;
-	}
-
-	const [major, minor] = version.split('.').map((s) => parseInt(s, 10));
-
-	return major < 11 || (major === 11 && minor === 0);
-};
-
-export function shellNeedsPatching(process: NodeJS.Process): boolean {
-	// We don't want to run this in the main process until the following issues
-	// are closed (and possibly not after they're closed either)
-	//
-	// See https://github.com/desktop/desktop/issues/13974
-	// See https://github.com/electron/electron/issues/32718
-	if (process.type === 'browser' && isMacOSCatalinaOrEarlier()) {
-		return false;
-	}
-
-	return process.platform === 'darwin' && !process.env.PWD;
-}
 
 // https://github.com/desktop/desktop/blob/development/app/src/lib/shell.ts#L37
 export async function updateEnvironmentForProcess(): Promise<void> {
