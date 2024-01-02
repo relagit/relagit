69da7db9776ae5d808d5116578a9e52f83fce3e5
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-01-02 14:44:49 +0100

    (stores) add stage.invert and move *stagedfilepaths methods

--- packages/app/src/stores/stage.ts
+++ packages/app/src/stores/stage.ts
@@ -1,5 +1,10 @@
 import { GenericStore } from '.';
 
+import FileStore from './files';
+import { Repository } from './repository';
+
+const nodepath = window.Native.DANGEROUS__NODE__REQUIRE('path');
+
 const StageStore = new (class StageStore extends GenericStore {
 	#record: Map<string, boolean> = new Map();
 
@@ -9,6 +14,34 @@ const StageStore = new (class StageStore extends GenericStore {
 
 	isStaged(filePath: string) {
 		return this.#record.has(filePath) ? this.#record.get(filePath) : true;
+	}
+
+	hasStagedFiles(path: string | undefined) {
+		return FileStore.getByRepositoryPath(path)?.some((f) =>
+			this.isStaged(nodepath.join(f.path, f.name))
+		);
+	}
+
+	getStagedFilePaths(path: string | undefined) {
+		return FileStore.getByRepositoryPath(path)
+			?.filter((f) => this.isStaged(nodepath.join(f.path, f.name)))
+			.map((f) => nodepath.join(f.path, f.name));
+	}
+
+	invert(repository: Repository | undefined) {
+		if (!repository) return;
+
+		const files = FileStore.getByRepositoryPath(repository.path);
+
+		console.log(files);
+
+		for (const file of files || []) {
+			console.log(nodepath.join(file.path, file.name));
+
+			this.toggleStaged(nodepath.join(file.path, file.name));
+		}
+
+		this.emit();
 	}
 
 	setStaged(filePath: string, staged: boolean) {
