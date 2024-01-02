e0cc4ca3d48e995aded2c99d0f2c93d9057bd021
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-01-02 14:45:00 +0100

    (modules/git/gitignore) fix type error

--- packages/app/src/modules/git/gitignore/index.ts
+++ packages/app/src/modules/git/gitignore/index.ts
@@ -4,7 +4,9 @@ import { Repository } from '@app/stores/repository';
 const path = window.Native.DANGEROUS__NODE__REQUIRE('path');
 const fs = window.Native.DANGEROUS__NODE__REQUIRE('fs');
 
-export const addToGitignore = (repo: Repository, fileOrExt: string) => {
+export const addToGitignore = (repo: Repository | undefined, fileOrExt: string) => {
+	if (!repo) return;
+
 	const isExt = fileOrExt.startsWith('.');
 	const gitignore = path.join(repo.path, '.gitignore');
 
