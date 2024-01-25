2ad321eccf285e57ec8704cb5c12e47206add92f
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-01-25 19:12:21 +1100

    (modules/git/clone) transition to repo

--- packages/app/src/modules/git/clone.ts
+++ packages/app/src/modules/git/clone.ts
@@ -1,3 +1,5 @@
+import LocationStore from '@app/stores/location';
+import RepositoryStore from '@app/stores/repository';
 import SettingsStore from '@app/stores/settings';
 
 import { Git } from './core';
@@ -17,6 +19,10 @@ export const Clone = async (url: string, directory: string) => {
 	]);
 
 	window._triggerWorkflow('repository_add', directory);
+
+	setTimeout(() => {
+		LocationStore.setSelectedRepository(RepositoryStore.getByPath(directory));
+	}, 500);
 
 	return result;
 };
