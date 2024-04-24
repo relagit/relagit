0ec91ea86cddd6f4b2afc0b7329209e0a3742f72
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-04-24 11:36:05 +1000

    (modules/ai/prompt) update ai prompt

--- packages/app/src/modules/ai/prompt.ts
+++ packages/app/src/modules/ai/prompt.ts
@@ -21,11 +21,11 @@ export const generatePrompt = async (repo: Repository) => {
 	const files: { diff: GitDiff; file: GitFile }[] = [];
 
 	let prompt =
-		'You are an AI embedded inside of a git client called relagit. Create a "COMMIT MESSAGE" from the following git diff of modified files. You will be presented with a list of files and their changed content and must only respond with a short and concise message summarizing the changes. Do not improvse or guess anything outside of what has been changed. A "+" before the line content indicates an added line, while a "-" indicates a removed line. Remember to take each chunk into account, chunks are separated by "--CHUNK--: ---"';
+		'Please create a git commit message from the following git diff of modified files. You must only respond with a short and concise message summarizing the changes. Do not improvise or guess anything outside of what has been changed. Here is the git diff:';
 
 	for (const path of paths) {
 		if (path.endsWith('.lock') || ignoredPaths.some((p) => path.includes(p))) {
-			prompt += `\n\n--FILE--: ${path} WAS CHANGED\n--ENDFILE--`;
+			prompt += `${path} is ignored and will should be included in the commit message.\n`;
 
 			continue;
 		}
@@ -34,13 +34,13 @@ export const generatePrompt = async (repo: Repository) => {
 		const diff = await Git.Diff(nodepath.join(repo.path, path), repo.path);
 
 		if (BINARY_EXTENSIONS.some((ext) => path.endsWith(ext))) {
-			prompt += `\n\n--FILE--: ${path} IS A BINARY FILE\n--ENDFILE--`;
+			prompt += `\n\n-- ${path} may not be included in the commit message as it is a binary file.\n`;
 
 			continue;
 		}
 
 		if (IMAGE_EXTENSIONS.some((ext) => path.endsWith(ext))) {
-			prompt += `\n\n--FILE--: ${path} IS AN IMAGE FILE\n--ENDFILE--`;
+			prompt += `\n\n-- ${path} was changed and is an image file.`;
 
 			continue;
 		}
@@ -48,30 +48,17 @@ export const generatePrompt = async (repo: Repository) => {
 		const parsed = parseDiff(diff);
 
 		if (diff === DIFF.ADD_ALL || diff === DIFF.REMOVE_ALL) {
-			prompt += `\n\n--FILE--: ${path}\n--CHANGES--: {--ALL_${
-				diff === DIFF.ADD_ALL ? 'ADDED' : 'REMOVED'
-			}--}: ${(await Git.Content(nodepath.join(repo.path, path), repo.path)).substring(
-				0,
-				400
-			)}\n--ENDFILE--`;
-
-			continue;
-		}
-
-		if (diff === DIFF.ADD_ALL) {
-			prompt += `\n\n--FILE--: ${path}\n--CHANGES--: {--ALL_ADDED--}: ${(
-				await Git.Content(nodepath.join(repo.path, path), repo.path)
-			).substring(0, 400)}\n--ENDFILE--`;
+			prompt += `\n\n-- ${path}:\n${(await Git.Content(nodepath.join(repo.path, path), repo.path)).substring(0, 400)}\n`;
 
 			continue;
 		}
 
 		files.push({ diff: parsed, file });
 
-		prompt += `\n\n--FILE--: ${path}\n--CHANGES--: \n`;
+		prompt += `\n\n-- ${path}:\n`;
 
 		for (const chunk of parsed.files[0]?.chunks || []) {
-			prompt += `--CHUNK--: ---\n${chunk.changes
+			prompt += `\n${chunk.changes
 				.map((c) => {
 					if (c.type === 'AddedLine') {
 						return `+ ${c.content}`;
@@ -80,42 +67,42 @@ export const generatePrompt = async (repo: Repository) => {
 					} else if (c.type !== 'MessageLine') {
 						return `  ${c.content}`;
 					}
+
+					return c.content;
 				})
-				.join('\n')}\n---\n`;
+				.join('\n')}\n`;
 		}
 
-		prompt += '\n--ENDFILE--';
+		prompt += '\n';
 	}
 
 	const parens = SettingsStore.settings.commit?.preferParens ? ['(', ')'] : ['[', ']'];
 
 	if (prompt.length > 4000) {
-		prompt = prompt.substring(0, 3000) + '---{...and more changes}---';
+		prompt = prompt.substring(0, 3000) + '-- There were too many changes to display. --';
 	}
 
 	prompt += `
---ENDPROMPT--
 Your answer MUST be in the following format:
 
 ${
 	SettingsStore.settings.commit?.styles?.[repo.path] === CommitStyle.relational ?
-		`${parens[0]}scope${parens[1]} <type> <description>`
+		`${parens[0]}scope${parens[1]} <type>: <description>`
 	:	`<type>${parens[0]}optional scope${parens[1]}: <description>`
 }
 [optional body]
 
-Some types that can be used:fix:, feat:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
+Some types that can be used:fix,feat,chore,ci,docs,style,refactor,perf,test,and others.
 The <type> and <description> fields are mandatory, the <scope> field is ${
 		SettingsStore.settings.commit?.styles?.[repo.path] === CommitStyle.relational ?
 			'required'
 		:	'optional'
-	}, and the (optional) <body> field is allowed.
-The <scope> should be the name(s) of files changed. e.g. if "/src/modules/ai/prompt.ts" was changed, the scope should be "modules/ai/prompt" or if "/src/modules/ai/prompt.ts" and "/src/modules/ai/index.ts" were changed, the scope should be "modules/ai".
-You may add a BODY ONLY if you feel the changes are complex and need more explanation. If you do, it must be separated from the description by a blank line.
-The BODY should be maximum 150 characters long. The COMMIT MESSAGE should be maximum 72 characters long.
-The COMMIT MESSAGE and BODY should be in the imperative tense, and describe what the commit does, not what it did. For example, use "change" instead of "changed" or "changes".
-The COMMIT MESSAGE must also be lowercase, and not end with a period.
-The BODY should not just be a repetition or summary of the description, but provide additional context. Note that the BODY is optional, and can be omitted if the description is sufficient.
+	}, and the <body> field is **optional**.
+The <scope> should be the name(s) of files changed. e.g. if "/src/modules/example/index.ts" was changed, the scope should be "modules/example" or if "/src/modules/example/test.ts" and "/src/modules/anotherexample/index.ts" were changed, the scope should be "modules", you MUST also include the parenthesis/brackets around the scope.
+You may add a body ONLY if you feel the changes are complex and need more explanation. If you do, it MUST be separated from the description by a blank line and MUST not just be a repetition or summary of the description, but provide additional context. Remember that the body is OPTIONAL, and can be omitted if the change is not big enough to warrant it.
+The body should be maximum 150 characters long. The summary should be maximum 72 characters long.
+The summary and body should be in the imperative tense, and describe what the commit does, not what it did. For example, use "change" instead of "changed" or "changes".
+The summary MUST also be lowercase, and not end with a period.
 `;
 
 	return prompt;
