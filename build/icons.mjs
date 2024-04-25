c4eb20c56fa174385db4a885e2b502d53b2e1be7
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-04-25 15:54:26 +1000

    (main) release: v0.14.8

--- build/icons.mjs
+++ build/icons.mjs
@@ -1,15 +1,13 @@
 import fs from 'fs/promises';
 import path from 'path';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const icons = path.resolve('../node_modules/vscode-material-icons/generated/icons/');
+const destination = path.resolve('../public/icons/');
 
-const icons = path.resolve(__dirname, '../node_modules/vscode-material-icons/generated/icons/');
-
-const destination = path.resolve(__dirname, '../public/icons/');
 try {
 	await fs.cp(icons, destination, { recursive: true });
 
 	console.log('Icons copied successfully!');
 } catch (error) {
-	console.error('Error copying icons:', error, (await fs.readdir(__dirname)).join(', '));
+	console.error('Error copying icons:', error);
 }
